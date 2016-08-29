32f9320eebe93c716f999f3e74f6cb4ee0fdc218
cmertz <chris@nimel.de>
2016-08-29 17:48:57 +0200

    add flag for http address

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+var addr = flag.String("addr", ":8080", "http service address")
+
 func main() {
+	flag.Parse()
+
 	results := make(chan result)
 
 	for i := 0; i < 5000; i++ {
@@ -15,5 +20,5 @@ func main() {
 	http.HandleFunc("/results", wsHandler(newResultChan(results)))
 	http.HandleFunc("/", newDashboard("/results"))
 
-	log.Fatal(http.ListenAndServe(":9090", nil))
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
